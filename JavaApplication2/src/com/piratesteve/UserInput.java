c7b3bda18cc649a946a312958c3699561edb8610
Steven <Steven@Pwnage>
2017-06-13 21:09:26 +0100

    changed the class name

---
+++ JavaApplication2/src/com/piratesteve/UserInput.java
@@ -0,0 +1,40 @@
+/*
+ * Just getting to grips with github
+ * 
+ * 
+ */
+package com.piratesteve;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Steven Barnes
+ */
+public class UserInput {
+
+    public static Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        System.out.println("What is you favourite number?");
+        int number = 0;
+        
+        try{
+            number = scan.nextInt();
+            scan.close();
+        }
+        catch(InputMismatchException e){
+            number = 7;
+            System.out.println("I'll choose for you then if you don't know what numbers are.");
+        }
+        finally{
+            System.out.println("Your favorite number is " + number);
+        }
+        
+        
+        
+    }
+    
+}
